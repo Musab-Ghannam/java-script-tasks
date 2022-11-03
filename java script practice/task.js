f031dd9cbcdc1b90289e90f070d6ee5b72ed65d9
Musab-Ghannam <115452464+Musab-Ghannam@users.noreply.github.com>
2022-11-03 05:57:05 -0700

    3 11 2022

---
+++ java script practice/task.js
@@ -0,0 +1,30 @@
+var m=-2;
+if(0<=m && m<50){
+document.write("FAIL");
+
+}else if (50<=m && m<60) {
+    document.write("D");
+  } else if (60<=m && m<70) {
+    document.write("c");
+  } 
+  else if (70<=m && m<80) {
+    document.write("B");
+  }  else if (80<=m && m<90) {
+    document.write("A");
+  }  else if (90<=m && m<=100) {
+    document.write("A+");
+  }  else if (m<0) {
+    document.write("NOT VAILD");
+  }  else if (m>100) {
+    document.write("NOT VAILD");
+  } 
+
+
+
+
+
+
+
+
+
+
---
+++ java script practice/task2.js
@@ -0,0 +1,6 @@
+const x=3.14
+
+
+
+z = x *5;
+document.write(z);
