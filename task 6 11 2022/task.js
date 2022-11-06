ae21bc5fc6762cffeb24613cbdceafba985ef986
Musab-Ghannam <115452464+Musab-Ghannam@users.noreply.github.com>
2022-11-06 15:41:47 -0800

    6 11

---
+++ task 6 11 2022/task.js
@@ -0,0 +1,25 @@
+let z =+prompt("insert your birthdate month");
+
+
+switch(z){
+case 8:
+    document.write("August"); break;
+
+    case 3:
+
+ document.write("March"); break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
